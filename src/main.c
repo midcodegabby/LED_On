16dfa4990b6241bd2a50204973f992a8a0eb10eb
midcodegabby <gabrielrodgers4i@gmail.com>
2024-08-06 12:06:23 -0700

    initialize new repository with led blinky functionality

--- src/main.c
+++ src/main.c
@@ -1,34 +1,65 @@
+//	Author: Gabriel Rodgers
+//	Date: 8/1/2024
+//	Purpose: To get the LD2 on the Nucleo-L476RG to turn on. 
+
 #include <stdint.h>
 
-//#include "clock.h"
-#include "gpio.h"
-
 #define GPIOA 0x48000000
-#define GPIOC 0x48000800
+#define RCC 0x40021000
 
 #define GPIOA_ODR (*((volatile uint32_t *) GPIOA + 0x14))
-#define GPIOC_IDR (*((volatile uint32_t *) GPIOC + 0x10))
+#define GPIOA_MODER (*((volatile uint32_t *) GPIOA))
+#define GPIOA_OTYPER (*((volatile uint32_t *) GPIOA + 0x04))
+#define GPIOA_OSPEEDR (*((volatile uint32_t *) GPIOA + 0x08))
+#define GPIOA_PUPDR (*((volatile uint32_t *) GPIOA + 0x0C))
+#define GPIOA_BSRR (*((volatile uint32_t *) GPIOA + 0x18))
+
+//define the first register in the RCC memory section
+//this method directly dereferences the memory itself to access the registers
+#define RCC_CR (*((volatile uint32_t *) RCC)) 	//sysclk config
+#define RCC_CFGR (*((volatile uint32_t *) RCC + 0x08))	//clk config
+#define RCC_AHB1ENR (*((volatile uint32_t *) RCC + 0x48))	//clk enable for peripherals
+#define RCC_AHB2ENR (*((volatile uint32_t *) RCC + 0x4C))	//clk enable for peripherals
+#define RCC_APB1ENR1 (*((volatile uint32_t *) RCC + 0x58))	//clk enable for peripherals
+#define RCC_CCIPR (*((volatile uint32_t *) RCC + 0x88))		//clk config for peripherals
+
+
 int main(void) {
-
-	//initialize all of the stuff
-	//sysclk_init();
-	//peripheral_clk_init();
-	//gpio_button_init();
-	gpio_led_init();
-
-	//uint32_t idr_contents;
-
-	//infinite loop checking if the button has been pressed
 	
-	while (1){
-		//read the value of the IDR register
-		//right shift GPIOC_IDR by 13, negate it, then & with one
-		//idr_contents = (~(GPIOC_IDR >> 13) & 1);
+	const uint32_t first = 1; 
 
-		//toggle LED: first mask the other bits of idr_contents
-		//if (idr_contents & (1 << 0)){
-		//	GPIOA_ODR ^= (1 << 5);
-		//}
+	//enable clk for peripherals (GPIO Port A)
+	RCC_AHB2ENR |= (1 << 0);	
+
+	for(uint32_t i=0; i < 100; i++); 
+
+	//set LED pin (GPIOA pin 5) to be general purpose output mode
+	GPIOA_MODER |= (1 << 10);
+	GPIOA_MODER &= ~(1 << 11);
+
+	//set LED pin to be Z (floating) so that led stays on
+	//GPIOA_PUPDR &= ~(1 << 11);
+	//GPIOA_PUPDR &= ~(1 << 10);
+
+	GPIOA_OTYPER &= ~(1 << 5); // set PA5 to be push-pull
+				   
+	GPIOA_ODR |= (1 << 5); // set led ON
+	//GPIOA_BSRR |= (1 << 5);
+
+	const uint32_t second = 1; 
+
+//	volatile uint32_t val;
+//	val = 0;
+
+	while (1) {
+		
+//		if (val == 1000000) {
+
+//			GPIOA_ODR ^= (1 << 5); // set led ON
+//			val = 0;
+//		}
+
+//		val++;
 	}
 	
 
